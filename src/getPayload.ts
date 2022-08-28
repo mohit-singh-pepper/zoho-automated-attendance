4d5b368842d9ea7856b4ac13bf99f4c075e8e67b
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2022-08-29 02:12:48 +0530

    fix: payloads

--- src/getPayload.ts
+++ src/getPayload.ts
@@ -1,38 +1,54 @@
 export const getPayload = (message: string, success: boolean) => ({
+	type: 'home',
 	blocks: [
 		{
 			type: 'section',
 			text: {
-				type: 'mrkdwn',
-				text: `You have a new request:\n*<https://people.zoho.in/peppercontentglobal/zp#home/dashboard|${
-					success ? 'Successful' : 'Failed'
-				}> ${message}*`,
+				type: 'plain_text',
+				text: (success ? ':white_check_mark: Successful' : ':x: Failed') + ' ' + message,
+				emoji: true,
 			},
 		},
+	],
+	attachments: [
 		{
-			type: 'actions',
-			elements: [
+			color: '#f2c744',
+			blocks: [
 				{
-					type: 'button',
+					type: 'section',
 					text: {
-						type: 'plain_text',
-						emoji: true,
-						text: 'Check-In Manually',
+						type: 'mrkdwn',
+						text: `You have a new request:\n*<https://people.zoho.in/peppercontentglobal/zp#home/dashboard|${
+							success ? 'Successful' : 'Failed'
+						}> ${message}*`,
 					},
-					style: success ? 'primary' : 'danger',
-					url: 'https://people.zoho.in/peppercontentglobal/zp#home/dashboard',
-					action_id: 'button-action-0',
 				},
 				{
-					type: 'button',
-					text: {
-						type: 'plain_text',
-						emoji: true,
-						text: 'Check-Out Manually',
-					},
-					style: success ? 'primary' : 'danger',
-					url: 'https://people.zoho.in/peppercontentglobal/zp#home/dashboard',
-					action_id: 'button-action-1',
+					type: 'actions',
+					elements: [
+						{
+							type: 'button',
+							text: {
+								type: 'plain_text',
+								emoji: true,
+								text: 'Check-In Manually',
+							},
+							style: success ? 'primary' : 'danger',
+							url: 'https://people.zoho.in/peppercontentglobal/zp#home/dashboard',
+							action_id: 'button-action-0',
+						},
+						{
+							type: 'button',
+							text: {
+								type: 'plain_text',
+								emoji: true,
+								text: 'Check-Out Manually',
+							},
+							style: success ? 'primary' : 'danger',
+							url: 'https://people.zoho.in/peppercontentglobal/zp#home/dashboard',
+							action_id: 'button-action-1',
+						},
+					],
 				},
 			],
 		},
